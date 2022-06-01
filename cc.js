812927f844792a1afb2956e6dca4c5dc767bbca9
szabolcsit <szabolcsit@hu.ibm.com>
2022-06-01 17:53:31 +0200

    update

--- cc.js
+++ cc.js
@@ -2,6 +2,8 @@
     const targetNode = document.getElementById('live-asr-target');
     const prevNode = document.getElementById('prev-asr-target');
     const prev2Node = document.getElementById('prev2-asr-target');
+    const prev3Node = document.getElementById('prev3-asr-target');
+    const prev4Node = document.getElementById('prev4-asr-target');
 
     const config = {
         characterData: true,
@@ -13,6 +15,8 @@
         for (let mutation of mutationList) {
             if (mutation.type === 'childList') {
                 for (let node of mutation.removedNodes) {
+                    prev4Node.textContent = prev3Node.textContent
+                    prev3Node.textContent = prev2Node.textContent
                     prev2Node.textContent = prevNode.textContent
                     prevNode.textContent = node.textContent;
                 }
